f41720e95db876c7b3afbcb527acfede04378c64
Bor Hodošček <bor.hodoscek@gmail.com>
2016-10-05 08:31:10 +0900

    removed unused endpoints and associated cleanup

--- src/cypress_editor/web_server.clj
+++ src/cypress_editor/web_server.clj
@@ -7,13 +7,8 @@
    [clojure.tools.logging :refer :all]
    [com.stuartsierra.component :refer [Lifecycle using]]
    [clojure.java.io :as io]
-   [cypress-editor.sources :refer [source-routes]]
-   ;;[cypress-editor.phonebook :refer [phonebook-routes]]
-   ;;[cypress-editor.phonebook-app :refer [phonebook-app-routes]]
-   [cypress-editor.hello :refer [hello-routes other-hello-routes]]
    [schema.core :as s]
    [selmer.parser :as selmer]
-   [yada.resources.webjar-resource :refer [new-webjar-resource]]
    [yada.yada :refer [handler resource] :as yada]))
 
 (defn content-routes []
@@ -26,12 +21,12 @@
        {:get
         {:produces #{"text/html"}
          :response (fn [ctx]
-                     (selmer/render-file "index.html" {:title "Edge Index"
+                     (selmer/render-file "index.html" {:title "Cypress"
                                                        :ctx ctx}))}}})]
 
     ["devcards.html"
      (yada/resource
-      {:id :cypress-editor.resources/index
+      {:id :cypress-editor.resources/devcards
        :methods
        {:get
         {:produces #{"text/html"}
@@ -52,39 +47,10 @@
   "Create the URI route structure for our application."
   [config]
   [""
-   [
-    ;; Hello World!
-    (hello-routes)
-    (other-hello-routes)
-
-    ;;(phonebook-routes db config)
-    ;;(phonebook-app-routes db config)
-
-    ["/api" (-> (hello-routes)
-                ;; Wrap this route structure in a Swagger
-                ;; wrapper. This introspects the data model and
-                ;; provides a swagger.json file, used by Swagger UI
-                ;; and other tools.
-                (yada/swaggered
-                 {:info {:title "Hello World!"
-                         :version "1.0"
-                         :description "An API on the classic example"}
-                  :basePath "/api"})
-                ;; Tag it so we can create an href to this API
-                (tag :cypress-editor.resources/api))]
-
-    ;; Swagger UI
-    ["/swagger" (-> (new-webjar-resource "/swagger-ui" {:index-files ["index.html"]})
-                    ;; Tag it so we can create an href to the Swagger UI
-                    (tag :cypress-editor.resources/swagger))]
-
-    ;; The Edge source code is served for convenience
-    (source-routes)
-
-    ;; Our content routes, and potentially other routes.
+   [;; Our content routes, and potentially other routes.
     (content-routes)
 
-    ;; This is a backstop. Always produce a 404 if we ge there. This
+    ;; This is a backstop. Always produce a 404 if we get there. This
     ;; ensures we never pass nil back to Aleph.
     [true (handler nil)]]])
 
