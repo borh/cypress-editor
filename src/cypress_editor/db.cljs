fb67437a6d820cbb27ede32a74b39963f15520d7
Bor Hodošček <bor.hodoscek@gmail.com>
2017-02-24 17:40:25 +0900

    added default api urls

--- src/cypress_editor/db.cljs
+++ src/cypress_editor/db.cljs
@@ -3,3 +3,12 @@
 (def sample-text
   ;; TODO add sample documents from Natane
   )
+
+#_"https://hinoki-project.org/p/natsume-server-api"
+(def base-url "nlp.lang.osaka-u.ac.jp/natsume-server/api")
+
+(def api-url
+  (str "https://" base-url))
+
+(def ws-url
+  (str "wss://" base-url))
