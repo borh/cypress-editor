12fafc740a9de090e491df7ed34c5f3b9c5bd344
Bor Hodošček <bor.hodoscek@gmail.com>
2017-02-24 17:40:42 +0900

    added missing subs

--- src/cypress_editor/subs.cljs
+++ src/cypress_editor/subs.cljs
@@ -1,42 +1,175 @@
 (ns cypress-editor.subs
-  (:require [re-frame.core :refer [reg-sub subscribe]]))
+  (:require [re-frame.core :refer [reg-sub]]))
+
+;; User API
 
 (reg-sub
- :input-text
+ :user/text
  (fn [db _]
-   (:input-text db)))
+   (:user/text db)))
 
 (reg-sub
- :input-text-state
+ :user/unit-type
  (fn [db _]
-   (:input-text-state db)))
+   (:user/unit-type db)))
 
 (reg-sub
- :input-text-results
+ :user/features
  (fn [db _]
-   (:input-text-results db)))
+   (:user/features db)))
 
 (reg-sub
- :user-text
+ :user/token
  (fn [db _]
-   (:user-text db)))
+   (:user/token db)))
 
 (reg-sub
- :error-data
+ :user/extra-token
  (fn [db _]
-   (:error-data db)))
+   (:user/extra-token db)))
 
 (reg-sub
- :text-topics
+ :user/genre
  (fn [db _]
-   (:text-topics db)))
+   (:user/genre db)))
 
 (reg-sub
- :selected-topic
+ :user/limit
  (fn [db _]
-   (:selected-topic db)))
+   (:user/limit db)))
 
 (reg-sub
- :connection-status
+ :user/html
  (fn [db _]
-   (:connection-status db)))
+   (:user/html db)))
+
+(reg-sub
+ :user/norm
+ (fn [db _]
+   (:user/norm db)))
+
+(reg-sub
+ :user/collocation
+ (fn [db _]
+   (:user/collocation db)))
+
+(reg-sub
+ :user/collocation-tree
+ (fn [db _]
+   (:user/collocation-tree db)))
+
+(reg-sub
+ :user/selected-topic
+ (fn [db _]
+   (:user/selected-topic db)))
+
+;; Server status API
+
+(reg-sub
+ :sente/connection-status
+ (fn [db _]
+   (:sente/connection-status db)))
+
+;; Backend API
+
+(reg-sub
+ :sources/genre
+ (fn [db _]
+   (:sources/genre db)))
+
+(reg-sub
+ :sentences/collocations
+ (fn [db _]
+   (:sentences/collocations db)))
+
+(reg-sub
+ :sentences/tokens
+ (fn [db _]
+   (:sentences/tokens db)))
+
+(reg-sub
+ :tokens/tree
+ (fn [db _]
+   (:tokens/tree db)))
+
+(reg-sub
+ :tokens/similarity
+ (fn [db _]
+   (:tokens/similarity db)))
+
+(reg-sub
+ :tokens/nearest-tokens
+ (fn [db _]
+   (:tokens/nearest-tokens db)))
+
+(reg-sub
+ :tokens/similarity-with-accuracy
+ (fn [db _]
+   (:tokens/similarity-with-accuracy db)))
+
+(reg-sub
+ :tokens/tsne
+ (fn [db _]
+   (:tokens/tsne db)))
+
+(reg-sub
+ :collocations/collocations
+ (fn [db _]
+   (:collocations/collocations db)))
+
+(reg-sub
+ :collocations/tree
+ (fn [db _]
+   (:collocations/tree db)))
+
+(reg-sub
+ :suggestions/tokens
+ (fn [db _]
+   (:suggestions/tokens db)))
+
+(reg-sub
+ :errors/register
+ (fn [db _]
+   (:errors/register db)))
+
+(reg-sub
+ :topics/infer
+ (fn [db _]
+   (:topics/infer db)))
+
+;; Fulltext API
+
+(reg-sub
+ :fulltext/query
+ (fn [db _]
+   (:fulltext/query db)))
+
+(reg-sub
+ :sentences/fulltext
+ (fn [db _]
+   (:sentences/fulltext db)))
+
+(reg-sub
+ :fulltext/genre-column
+ (fn [db _]
+   (:fulltext/genre-column db)))
+
+(reg-sub
+ :fulltext/title-column
+ (fn [db _]
+   (:fulltext/title-column db)))
+
+(reg-sub
+ :fulltext/author-column
+ (fn [db _]
+   (:fulltext/author-column db)))
+
+(reg-sub
+ :fulltext/year-column
+ (fn [db _]
+   (:fulltext/year-column db)))
+
+(reg-sub
+ :fulltext/state
+ (fn [db _]
+   (:fulltext/state db)))
