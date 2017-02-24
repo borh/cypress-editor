3c1b9b2098cbabfa1c9af04136f7779b0191ee38
Bor Hodošček <bor.hodoscek@gmail.com>
2017-02-24 17:37:32 +0900

    added new interface endpoint
    This is temporary and the two will be merged in the future.

--- src/cypress_editor/app.cljs
+++ src/cypress_editor/app.cljs
@@ -1,22 +1,29 @@
 (ns cypress-editor.app
   (:require
    ;; Do not remove the following requires:
-   [cypress-editor.events]
    [cypress-editor.subs]
+   [cypress-editor.events]
    ;;
 
    [cypress-editor.views :refer [interface]]
+   [cypress-editor.re-com-views :as rcv]
 
    [reagent.core :as r]
-   [re-frame.core :refer [dispatch-sync]]))
+   [re-frame.core :refer [dispatch-sync]]
+   [re-frisk.core :refer [enable-re-frisk!]]))
 
 ;; # App entry point
+
+(enable-console-print!)
 
 (def ^boolean debug-enabled? "@define {boolean}" ^boolean js/goog.DEBUG)
 
 (defn ^:export main
   []
-  (enable-console-print!)
   (dispatch-sync [:boot])
-  (r/render [interface]
+  (when debug-enabled?
+    (enable-console-print!)
+    (println "Debug mode enabled...")
+    (enable-re-frisk! {:x 50 :y 120}))
+  (r/render [rcv/interface]
             (. js/document (getElementById "app"))))
