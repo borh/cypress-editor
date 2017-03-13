e88067e1acab905b69ec2679fad77d81b51f3e6d
Bor Hodošček <bor.hodoscek@gmail.com>
2017-03-13 22:28:50 +0900

    made sente socket an atom

--- src/cypress_editor/communication.cljs
+++ src/cypress_editor/communication.cljs
@@ -1,29 +1,38 @@
 (ns cypress-editor.communication
   (:require [taoensso.sente :as sente]
             [taoensso.sente.packers.transit :as sente-transit]
-            [mount.core :refer [defstate]]
-            [cypress-editor.db :refer [ws-url]]))
+            [re-frame.core :refer [dispatch]]
+            [cypress-editor.db :refer [ws-url api-url]]))
 
-(defstate socket
-  :start (sente/make-channel-socket!
-          "/chsk"
-          {:type :auto
-           :packer (sente-transit/get-transit-packer)
-           :chsk-url-fn #(str ws-url %)}))
+;; https://github.com/ptaoussanis/sente/issues/118#issuecomment-87378277
+
+(def !socket (atom))
 
 (defn send! [& params]
-  (apply (:send-fn @socket) params))
+  (apply (:send-fn @!socket) params))
 
 (defmulti handle-message first)
 
 (defmulti handle-event :id)
 
-(defmethod handle-event :chsk/state [_])
+(defmethod handle-event :chsk/state [{:keys [state]}]
+  (dispatch [:set/sente-connection-status (:open? @state)]))
 
 (defmethod handle-event :chsk/handshake [_])
 
-(defmethod handle-event :chsk/recv [{:keys [?data]}]
+(defmethod handle-event :chsk/recv [{:keys [?data ?csrf-token]}]
   (handle-message ?data))
 
-(defstate router
-  :start (sente/start-chsk-router! (:ch-recv @socket) handle-event))
+(defmethod handle-message :chsk/ws-ping [_]
+  (dispatch [:set/sente-connection-status (:open? @(:state @!socket))]))
+
+(defn create-socket! [token]
+  (reset! !socket
+          (sente/make-channel-socket!
+           "/chsk"
+           {:type :auto
+            :packer (sente-transit/get-transit-packer)
+            :client-id token
+            :params {:uid "uuid"}
+            :chsk-url-fn #(str ws-url %)}))
+  (sente/start-chsk-router! (:ch-recv @!socket) handle-event))
