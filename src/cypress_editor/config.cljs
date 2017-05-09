cec57d83c4cd71c4ee5a4a680774a2c7e6f0a91c
Bor Hodošček <bor.hodoscek@gmail.com>
2017-05-09 20:49:06 +0900

    moved db ns to more apt config ns

--- src/cypress_editor/config.cljs
+++ src/cypress_editor/config.cljs
@@ -1,12 +1,7 @@
-(ns cypress-editor.db)
+(ns cypress-editor.config)
 
 (def ^boolean debug-enabled? "@define {boolean}" ^boolean js/goog.DEBUG)
 
-(def sample-text
-  ;; TODO add sample documents from Natane
-  )
-
-#_"https://hinoki-project.org/p/natsume-server-api"
 (def base-url "nlp.lang.osaka-u.ac.jp/natsume-server/api")
 
 (def api-url
