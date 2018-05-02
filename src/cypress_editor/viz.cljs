d6d3c22ba6f67b64fc34adf4680fbabd1330f378
Bor Hodošček <bor.hodoscek@gmail.com>
2018-05-02 15:07:07 +0900

    refactored to use rid3-> macro

--- src/cypress_editor/viz.cljs
+++ src/cypress_editor/viz.cljs
@@ -1,5 +1,5 @@
 (ns cypress-editor.viz
-  (:require [rid3.core :as rid3]))
+  (:require [rid3.core :as rid3 :refer [rid3->]]))
 
 ;;
 
@@ -27,32 +27,32 @@
 ;;
 
 (defn main-container-did-mount [node ratom]
-  (-> node
-      (.attr "transform" (str "translate("
-                              (get margin :left)
-                              ","
-                              (get margin :top)
-                              ")"))))
+  (rid3-> node
+          {:transform (str "translate("
+                           (get margin :left)
+                           ","
+                           (get margin :top)
+                           ")")}))
 
 (defn svg-did-mount [node ratom]
-  (-> node
-      (.attr "width" (+ (get-width ratom)
-                        (get margin :left 0)
-                        (get margin :right 0)))
-      (.attr "height" (+ (get-height ratom)
-                         (get margin :top 0)
-                         (get margin :bottom 0)))))
+  (rid3-> node
+          {:width (+ (get-width ratom)
+                     (get margin :left 0)
+                     (get margin :right 0))
+           :height (+ (get-height ratom)
+                      (get margin :top 0)
+                      (get margin :bottom 0))}))
 
 (defn svg-did-update [node ratom]
-  (-> node
-      (.transition)
-      (.duration transition-duration)
-      (.attr "width" (+ (get-width ratom)
-                        (get margin :left 0)
-                        (get margin :right 0)))
-      (.attr "height" (+ (get-height ratom)
-                         (get margin :top 0)
-                         (get margin :bottom 0)))))
+  (rid3-> node
+          (.transition)
+          (.duration transition-duration)
+          {:width (+ (get-width ratom)
+                     (get margin :left 0)
+                     (get margin :right 0))
+           :height (+ (get-height ratom)
+                      (get margin :top 0)
+                      (get margin :bottom 0))}))
 
 (defn bar-did-mount [node ratom]
   (let [width       (get-width ratom)
@@ -60,19 +60,19 @@
         data-n      (count @ratom)
         rect-height (/ height data-n)
         color (.scaleOrdinal js/d3 (.-schemeCategory10 js/d3))]
-    (-> node
-        (.style "shape-rendering" "crispEdges")
-        (.attr "fill" "black" #_(fn [d] (color (aget d "idx"))))
-        (.attr "x" (x-scale ratom 0))
-        (.attr "y" (fn [_ i]
-                     (* i rect-height)))
-        (.attr "height" (- rect-height 1))
-        (.attr "opacity" 0.2)
-        (.transition)
-        (.duration transition-duration)
-        (.attr "width" (fn [d]
-                         (x-scale ratom (aget d "frequency"))))
-        (.attr "opacity" 1))))
+    (rid3-> node
+            (.style "shape-rendering" "crispEdges")
+            {:fill "black"
+             :x (x-scale ratom 0)
+             :y (fn [_ i]
+                  (* i rect-height))
+             :height (- rect-height 1)
+             :opacity 0.2}
+            (.transition)
+            (.duration transition-duration)
+            {:width (fn [d]
+                      (x-scale ratom (aget d "frequency")))
+             :opacity 1})))
 
 (defn bar-did-update [node ratom]
   (let [width       (get-width ratom)
@@ -80,34 +80,34 @@
         data-n      (count @ratom)
         rect-height (/ height data-n)
         color (.scaleOrdinal js/d3 (.-schemeCategory10 js/d3))]
-    (-> node
-        (.style "shape-rendering" "crispEdges")
-        (.attr "fill" "black" #_(fn [d] (color (aget d "idx"))))
-        (.attr "opacity" 0.8)
-        (.attr "y" (fn [_ i]
-                     (* i rect-height)))
-        (.transition)
-        (.duration transition-duration)
-        (.attr "opacity" 1)
-        (.attr "height" (- rect-height 1))
-        (.attr "x" (x-scale ratom 0))
-        (.attr "width" (fn [d]
-                         (x-scale ratom (aget d "frequency")))))))
+    (rid3-> node
+            (.style "shape-rendering" "crispEdges")
+            (.attr "fill" "black" #_(fn [d] (color (aget d "idx"))))
+            (.attr "opacity" 0.8)
+            (.attr "y" (fn [_ i]
+                         (* i rect-height)))
+            (.transition)
+            (.duration transition-duration)
+            (.attr "opacity" 1)
+            (.attr "height" (- rect-height 1))
+            (.attr "x" (x-scale ratom 0))
+            (.attr "width" (fn [d]
+                             (x-scale ratom (aget d "frequency")))))))
 
 (defn bar-label-common [node ratom text-key text-color text-size]
   (let [width       (get-width ratom)
         height      (get-height ratom)
         data-n      (count @ratom)
         rect-height (/ height data-n)]
-    (-> node
-        (.attr "y" (fn [_ i]
-                     (+ (* i rect-height)
-                        (/ rect-height 2))))
-        (.attr "alignment-baseline" "middle")
-        (.attr "fill" text-color)
-        (.attr "font-family" "sans-serif")
-        (.attr "font-size" text-size)
-        (.text (fn [d] (aget d text-key))))))
+    (rid3-> node
+            {:y (fn [_ i]
+                  (+ (* i rect-height)
+                     (/ rect-height 2)))
+             :alignment-baseline "middle"
+             :fill text-color
+             :font-family "sans-serif"
+             :font-size text-size}
+            (.text (fn [d] (aget d text-key))))))
 
 (defn bar-freq-label-did-mount [node ratom]
   (let [width (get-width ratom)]
